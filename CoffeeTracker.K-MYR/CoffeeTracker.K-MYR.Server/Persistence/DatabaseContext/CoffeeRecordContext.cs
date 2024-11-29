eeaf34eee6d22f379bfc7841fe4768f4b7eef6d6
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-11-29 07:58:57 +0100

    feat: Create projects and add crud operations for the api

---
+++ CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Server/Persistence/DatabaseContext/CoffeeRecordContext.cs
@@ -0,0 +1,9 @@
+﻿using CoffeeTracker.K_MYR.Server.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeTracker.K_MYR.Server.Persistence.DatabaseContext;
+
+public sealed class CoffeeRecordContext(DbContextOptions<CoffeeRecordContext> options) : DbContext(options)
+{
+    public required DbSet<CoffeeRecord> CoffeeRecords { get; set; }
+}
