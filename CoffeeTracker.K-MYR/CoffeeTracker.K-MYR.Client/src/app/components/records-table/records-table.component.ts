ff0a1615d1c978ce5b528ab25c232215c69ea486
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-12-22 21:06:30 +0100

    feat: add record-modal component

--- CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/records-table/records-table.component.ts
+++ CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/records-table/records-table.component.ts
@@ -1,18 +1,19 @@
 import { CoffeeRecord } from '../../interfaces/coffee-record';
+import { AddRecordModalComponent } from '../add-record-modal/add-record-modal.component';
 
-import { Component } from '@angular/core';
+import { Component, Signal, viewChild } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-records-table',
   standalone: true,
-  imports: [ DatePipe, ],
+  imports: [ DatePipe, AddRecordModalComponent],
   templateUrl: './records-table.component.html',
   styleUrl: './records-table.component.scss'
 })
 export class RecordsTableComponent {
-  private _isAscending: boolean = true;
+  private dialog: Signal<AddRecordModalComponent> = viewChild.required<AddRecordModalComponent>('addRecordModal');
   private _currentColumn: keyof CoffeeRecord = 'id';
   records: CoffeeRecord[] = [
     { id: 1, dateTime: new Date('2024-11-17 09:00:00'), type: 'Iced Coffee' },
@@ -22,5 +23,9 @@ export class RecordsTableComponent {
     { id: 2, dateTime: new Date('2024-12-17 09:00:00'), type: 'Espresso' },
     { id: 3, dateTime: new Date('2024-12-18 09:00:00'), type: 'Caffe Latte' },
     { id: 4, dateTime: new Date('2024-12-19 09:00:00'), type: 'Cappucino' },
-  ];  
+  ];
+
+  openAddRecordModal(): void {
+    this.dialog().open();
+  }
 }
