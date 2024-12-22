5452b100801b320898099e78b946f5eddef04755
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-12-22 21:06:30 +0100

    feat: add record-modal component

--- CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/add-record-modal/add-record-modal.component.ts
+++ CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/add-record-modal/add-record-modal.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Signal, viewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-record-modal',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, ],
   templateUrl: './add-record-modal.component.html',
   styleUrl: './add-record-modal.component.scss'
 })
 export class AddRecordModalComponent {
+  private dialog: Signal<ElementRef<HTMLDialogElement>> = viewChild.required<ElementRef<HTMLDialogElement>>('dialog');
+  recordForm: FormGroup = new FormGroup({
+    type: new FormControl(''),
+    dateTime: new FormControl('')
+  });
 
+  open() : void {
+    this.dialog().nativeElement.showModal();
+  }
+
+  close(): void {
+    this.dialog().nativeElement.close();
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    if (event.target == this.dialog().nativeElement) {
+      this.close();
+    };
+  } 
 }
