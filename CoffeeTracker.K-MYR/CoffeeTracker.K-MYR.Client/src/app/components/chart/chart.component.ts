dd02b0b85208575f0b6c2872b594b8f0565cdec9
K-MYR <144546255+K-MYR@users.noreply.github.com>
2025-01-20 22:23:37 +0100

    feat: add chart component

--- CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/chart/chart.component.ts
+++ CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Client/src/app/components/chart/chart.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
+  imports: [ BaseChartDirective ],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss'
 })
 export class ChartComponent {
-
+  chartData = input.required<ChartData>();
+  chartOptions = input.required<ChartOptions>();  
 }
