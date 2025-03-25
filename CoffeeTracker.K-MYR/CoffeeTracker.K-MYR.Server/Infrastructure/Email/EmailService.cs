60a1d770a88c84aed7e705e7e096f44b4a4b6b54
K-MYR <144546255+K-MYR@users.noreply.github.com>
2025-03-25 14:01:47 +0100

    feat: add new logo and brand to ui and email

--- CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Server/Infrastructure/Email/EmailService.cs
+++ CoffeeTracker.K-MYR/CoffeeTracker.K-MYR.Server/Infrastructure/Email/EmailService.cs
@@ -18,11 +18,34 @@ public class EmailService(IOptions<EmailConfiguration> emailConfiguration, IRazo
         MimeMessage message = new();
         message.From.Add(new MailboxAddress(_configuration.SenderName, _configuration.SenderEmail));
         message.To.Add(new MailboxAddress(user.UserName, email));
-        message.Subject = "Welcome to CoffeeTracker! Please Confirm Your Email";     
-        message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+        message.Subject = "Welcome to CoffeeTracker! Please Confirm Your Email";
+
+        var body = await _renderer.RenderViewToStringAsync("Views/Emails/EmailConfirmation.cshtml", new EmailConfirmation(confirmationLink));
+        var builder = new BodyBuilder()
         {
-            Text = await _renderer.RenderViewToStringAsync("/Views/Emails/EmailConfirmation.cshtml", new EmailConfirmation(confirmationLink))
-        };        
+            HtmlBody = body
+        };
+
+        var brandPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Images", "coffee-tracker-brand-with-glow.png");
+        var brand = new MimePart("image", "png")
+        {
+            Content = new MimeContent(File.OpenRead(brandPath)),
+            ContentId = "brand",
+            ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
+            ContentTransferEncoding = ContentEncoding.Base64
+        };
+        builder.LinkedResources.Add(brand);
+        var logoPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Images", "coffee-logo-with-glow.png");
+        var logo = new MimePart("image", "png")
+        {
+            Content = new MimeContent(File.OpenRead(logoPath)),
+            ContentId = "logo",
+            ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
+            ContentTransferEncoding = ContentEncoding.Base64
+        };
+        builder.LinkedResources.Add(logo);
+        message.Body = builder.ToMessageBody();         
+        
         using var client = new SmtpClient();
         await client.ConnectAsync(_configuration.SmtpServer, _configuration.SmtpPort);       
         await client.SendAsync(message);
