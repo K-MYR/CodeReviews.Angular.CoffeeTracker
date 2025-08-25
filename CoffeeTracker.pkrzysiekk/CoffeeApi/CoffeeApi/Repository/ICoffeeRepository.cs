a53e2d428091c5b817f147242953fccb5dabf22d
pkrzysiek <pkrzysiek24@gmail.com>
2025-08-25 14:53:47 +0200

    Add generic repository

---
+++ CoffeeTracker.pkrzysiekk/CoffeeApi/CoffeeApi/Repository/ICoffeeRepository.cs
@@ -0,0 +1,11 @@
+namespace CoffeeApi.Repository;
+
+public interface IRepository<T> where T : class
+{
+   public Task Create(T item);
+   public Task Update(T item);
+   public Task Delete(T item);
+   public Task<T?> GetById(int id);
+   public IQueryable<T> GetAll();
+   public Task SaveChanges();
+}
