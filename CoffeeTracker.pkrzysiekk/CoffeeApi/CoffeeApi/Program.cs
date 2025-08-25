fa7376e98b3b74e1529abaafeb88692cca6347ca
pkrzysiek <pkrzysiek24@gmail.com>
2025-08-25 14:52:33 +0200

    Modify Program.cs

---
+++ CoffeeTracker.pkrzysiekk/CoffeeApi/CoffeeApi/Program.cs
@@ -0,0 +1,40 @@
+using CoffeeApi.Data;
+using CoffeeApi.Models;
+using CoffeeApi.Repository;
+using CoffeeApi.Service;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CoffeeContext>(options => options.
+    UseSqlite("Data Source=coffee.db"));
+builder.Services.AddScoped<CoffeeContext>();
+builder.Services.AddScoped<ICoffeeService, CoffeeService>();
+builder.Services.AddScoped<IRepository<CoffeeConsumption>, CoffeeRepository>();
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<CoffeeContext>();
+    await context.Database.EnsureCreatedAsync();
+} 
+app.UseSwagger();
+app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
