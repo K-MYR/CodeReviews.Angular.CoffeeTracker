8c64741869cd10cd56e2cabd0cb903eb9a138bef
pkrzysiek <pkrzysiek24@gmail.com>
2025-08-25 16:22:01 +0200

    Add Coffee module and CRUD components

---
+++ CoffeeTracker.pkrzysiekk/CoffeeFront/src/app/coffee/coffee.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoffeeRoutingModule } from './coffee-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoffeeRoutingModule
+  ]
+})
+export class CoffeeModule { }
