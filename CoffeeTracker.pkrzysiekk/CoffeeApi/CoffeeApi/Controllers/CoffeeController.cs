72904260940b29370e4ec35c63779c81c5875d84
pkrzysiek <pkrzysiek24@gmail.com>
2025-08-25 14:54:52 +0200

    Implement CoffeeController.cs

---
+++ CoffeeTracker.pkrzysiekk/CoffeeApi/CoffeeApi/Controllers/CoffeeController.cs
@@ -0,0 +1,46 @@
+using CoffeeApi.Data;
+using CoffeeApi.Models;
+using CoffeeApi.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly ICoffeeService  _coffeeService;
+
+        public CoffeeController(ICoffeeService coffeeService)
+        {
+            _coffeeService = coffeeService;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<CoffeeConsumption>> 
+            GetPaginatedResult([FromQuery] int page, [FromQuery] int size)
+        {
+            var paginatedResult= await 
+                _coffeeService.GetPaginatedList(page, size);
+            return paginatedResult;
+        }
+
+        [HttpPost]
+        public async Task Create([FromBody] CoffeeConsumption coffeeConsumption)
+        {
+            await _coffeeService.Add(coffeeConsumption);
+        }
+
+        [HttpPut]
+        public async Task Update( [FromBody] CoffeeConsumption coffeeConsumption)
+        {
+            await _coffeeService.Update(coffeeConsumption);
+        }
+
+        [HttpDelete]
+        public async Task Delete( [FromBody] CoffeeConsumption coffeeConsumption )
+        {
+            await _coffeeService.Delete(coffeeConsumption);
+        }
+    }
+}
