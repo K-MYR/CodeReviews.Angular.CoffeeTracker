459d6591f8ef9b92968810296550dad4e918161e
pkrzysiek <pkrzysiek24@gmail.com>
2025-08-27 19:47:50 +0200

    Add jsonProperty

--- CoffeeTracker.pkrzysiekk/CoffeeApi/CoffeeApi/Models/CoffeeConsumption.cs
+++ CoffeeTracker.pkrzysiekk/CoffeeApi/CoffeeApi/Models/CoffeeConsumption.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace CoffeeApi.Models;
 
 public class CoffeeConsumption
 {
     public int Id { get; set; }
-    public required string  TypeOfCoffee { get; set; }
+    [JsonPropertyName("typeOfCoffee")]
+    public required string  TypeOfCoffee { get; set; } 
+    [JsonPropertyName("consumedAt")]
     public DateTime ConsumedAt { get; set; }
+    [JsonPropertyName("caffeine")]
     public int Caffeine { get; set; }
 }
